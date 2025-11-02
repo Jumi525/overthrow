57a2fa7ce8d966bccf2fae51974ecfd159f92887
Jumi525 <jumiklein525@gmail.com>
2025-11-02 23:24:26 +0100

    second overthrow commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
+  build: {
+    target: "esnext",
+  },
+  // 🚫 Disable strict type checking in build
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
+});
