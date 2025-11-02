32092d94636c39753b0249cefa1b2cdd815634e7
Jumi525 <jumiklein525@gmail.com>
2025-11-02 23:51:46 +0100

    second overthrow commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      phaser: "phaser", // ensure it's treated as an npm package, not a local file
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: [], // leave empty unless you explicitly want to exclude a dependency
-    },
-  },
 });
